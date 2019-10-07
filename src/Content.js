3811b50de41910b3a2b95026d983acdb76bf3857
Joe Martella <martellaj@live.com>
2019-10-07 07:32:00 -0700

    Adds FeatureCard component

--- src/Content.js
+++ src/Content.js
@@ -1,10 +1,50 @@
 import React from "react";
 import "./Content.css";
+import FeatureCard from "./FeatureCard";
 
 function Content(props) {
+    const getContentTitle = selectedMenuItem => {
+        switch (selectedMenuItem) {
+            case 0:
+                return "Basic Features";
+            case 1:
+                return "Premium Features";
+            case 2:
+                return "FAQ";
+            default:
+                return "Contact";
+        }
+    };
+
+    const getContent = selectedMenuItem => {
+        switch (selectedMenuItem) {
+            case 0:
+                return [<FeatureCard />, <FeatureCard />];
+            case 1:
+                return [
+                    <FeatureCard />,
+                    <FeatureCard />,
+                    <FeatureCard />,
+                    <FeatureCard />
+                ];
+            case 2:
+                return [<FeatureCard />];
+            default:
+                return [<FeatureCard />];
+        }
+    };
+
     return (
         <div className="Content--container">
-            MenuItem {props.selectedMenuItem} selected
+            <h2 className="Content--title">
+                {getContentTitle(props.selectedMenuItem)}
+            </h2>
+            <div className="Content--content">
+                {getContent(props.selectedMenuItem)}
+
+                {/* Spacing block */}
+                <div style={{ height: "24px" }}></div>
+            </div>
         </div>
     );
 }
---
+++ src/FeatureCard.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./FeatureCard.css";
+
+function FeatureCard(props) {
+    return <div className="FeatureCard--container"></div>;
+}
+
+export default FeatureCard;
