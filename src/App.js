704d946ad620f85371dbdabc9bc6829ed70a51fe
Joe Martella <martellaj@live.com>
2019-10-07 06:46:21 -0700

    Removes extra character

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     return (
         <div className="container">
             <Menu onMenuItemClicked={onMenuItemClicked} />
-            <Content selectedMenuItem={selectedMenuItem} />>
+            <Content selectedMenuItem={selectedMenuItem} />
         </div>
     );
 }
