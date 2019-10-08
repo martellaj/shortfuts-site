058f4a0836ef4a73cbcfeebf8875acee1aee47a6
Joe Martella <martellaj@live.com>
2019-10-07 23:34:54 -0700

    Adds routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Menu from "./Menu";
 import Content from "./Content";
@@ -6,6 +6,20 @@ import Header from "./Header";
 
 function App() {
     const [selectedMenuItem, setSelectedMenuItem] = useState(0);
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(window.location.search);
+
+        const premiumParam = urlParams.get("premium");
+        if (premiumParam) {
+            setSelectedMenuItem(1);
+        }
+
+        const faqParam = urlParams.get("faq");
+        if (faqParam) {
+            setSelectedMenuItem(2);
+        }
+    }, []);
 
     const onMenuItemClicked = index => {
         return () => setSelectedMenuItem(index);
--- src/FeatureCard.js
+++ src/FeatureCard.js
@@ -6,7 +6,19 @@ function FeatureCard(props) {
 
     return (
         <div className="FeatureCard--container">
-            <div className="FeatureCard--header">{title}{demo && <a style={{marginLeft: '12px'}} href={demo} target="_blank">FEATURE DEMO</a>}</div>
+            <div className="FeatureCard--header">
+                {title}
+                {demo && (
+                    <a
+                        style={{ marginLeft: "12px" }}
+                        href={demo}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        FEATURE DEMO
+                    </a>
+                )}
+            </div>
             <div className="FeatureCard--description">{description}</div>
         </div>
     );
