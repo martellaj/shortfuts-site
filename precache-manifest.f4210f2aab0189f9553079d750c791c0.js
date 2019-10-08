aadf4e49b6b41863cff5f74feb9f2b974247126d
Joe Martella <martellaj@live.com>
2019-10-07 23:40:27 -0700

    Updates

---
+++ precache-manifest.f4210f2aab0189f9553079d750c791c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a8a7391c1cec1491365534b5726d5fb",
+    "url": "/shortfuts-site/index.html"
+  },
+  {
+    "revision": "4d97a84c4534506e53ea",
+    "url": "/shortfuts-site/static/css/main.2273fe55.chunk.css"
+  },
+  {
+    "revision": "ac0c3c920ac59b048ed3",
+    "url": "/shortfuts-site/static/js/2.006d39b8.chunk.js"
+  },
+  {
+    "revision": "4d97a84c4534506e53ea",
+    "url": "/shortfuts-site/static/js/main.db4be606.chunk.js"
+  },
+  {
+    "revision": "c315b5daf08c8dc54698",
+    "url": "/shortfuts-site/static/js/runtime-main.bb9a0287.js"
+  }
+]);
